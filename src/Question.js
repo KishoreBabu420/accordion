8162f21dbdbf8ad64baaf6a34eba4bbf5bc88cb4
Kishore <kishore.sai37@gmail.com>
2021-07-14 22:10:28 +0530

    update data

--- src/Question.js
+++ src/Question.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = ({ title, info }) => {
+const Question = ({ question, answer }) => {
   const [showInfo, setShowInfo] = useState(false);
   return (
     <article className='question'>
       <header>
-        <h4>{title}</h4>
+        <h4>{question}</h4>
         <button className='btn' onClick={() => setShowInfo(!showInfo)}>
           {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
-      {showInfo && <p>{info}</p>}
+      {showInfo && <p>{answer}</p>}
     </article>
   );
 };
--- src/data.js
+++ src/data.js
@@ -1,33 +1,28 @@
 const questions = [
   {
     id: 1,
-    title: 'Do I have to allow the use of cookies?',
-    info:
-      'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
+    question: 'What are your weaknesses?',
+    answer: `What are your weaknesses" is one of the most popular questions interviewers ask. It is also the most dreaded question of all. Handle it by minimizing your weakness and emphasizing your strengths. Stay away from personal qualities and concentrate on professional traits: "I am always working on improving my communication skills to be a more effective presenter. I recently joined Toastmasters, which I find very helpful.`,
   },
   {
     id: 2,
-    title: 'How do I change my My Page password?',
-    info:
-      'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
+    question: 'Why should we hire you?',
+    answer: `Why should we hire you?" by summarizing your experiences: "With five years' experience working in the financial industry and my proven record of saving the company money, I could make a big difference in your company. I'm confident I would be a great addition to your team.`,
   },
   {
     id: 3,
-    title: 'What is BankID?',
-    info:
-      'Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.',
+    question: 'Why do you want to work here?',
+    answer: `By asking you, "Why do you want to work here?" the interviewer is listening for an answer that indicates you've given this some thought and are not sending out resumes just because there is an opening. For example, "I've selected key companies whose mission statements are in line with my values, where I know I could be excited about what the company does, and this company is very high on my list of desirable choices.`,
   },
   {
     id: 4,
-    title: 'Whose birth number can I use?',
-    info:
-      'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.',
+    question: 'What are your goals?',
+    answer: `When you're asked, "What are your goals?" sometimes it's best to talk about short-term and intermediate goals rather than locking yourself into the distant future. For example, "My immediate goal is to get a job in a growth-oriented company. My long-term goal will depend on where the company goes. I hope to eventually grow into a position of responsibility."`,
   },
   {
     id: 5,
-    title: 'When do I recieve a password ordered by letter?',
-    info:
-      'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
+    question: 'Why did you leave (or why are you leaving) your job?',
+    answer: `One of the most critical job interview tips: Don't badmouth a former employer. So if an interviewer asks, "Why did you leave (or why are you leaving) your job?" and you're unemployed, state your reason for leaving in a positive context: "I managed to survive two rounds of corporate downsizing, but the third round was a 20% reduction in the workforce, which included me."`,
   },
-]
-export default questions
+];
+export default questions;
